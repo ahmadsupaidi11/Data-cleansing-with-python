0e66c699f51a1614fe5a0c24498990d36b6c436f
ahmadsupaidi11 <45868213+ahmadsupaidi11@users.noreply.github.com>
2020-07-26 10:50:36 +0700

    siap

--- data cleansing.py
+++ data cleansing.py
@@ -1,11 +1,84 @@
 import  pandas as pd
+import  matplotlib.pyplot as plt
+import seaborn as sns
 pd.options.display.max_columns = 50
 
 df_load = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/dqlab_telco.csv')
 print(df_load.shape)
 print(df_load.head(5))
 print(df_load.customerID.nunique())
-
+#filter id pelanggan
 df_load['valid_id'] = df_load['customerID'].astype(str).str.match(r'(45\d{9,10})')
 df_load = (df_load[df_load['valid_id'] == True]).drop('valid_id', axis = 1)
 print('Hasil jumlah ID Customer yang terfilter adalah',df_load['customerID'].count())
+
+# Drop Duplicate Rows
+df_load.drop_duplicates()
+# Drop duplicate ID sorted by Periode
+df_load = df_load.sort_values('UpdatedAt', ascending=False).drop_duplicates(['customerID'])
+print('Hasil jumlah ID Customer yang sudah dihilangkan duplikasinya (distinct) adalah',df_load['customerID'].count())
+
+#menghapus data null
+print('total mising value data pada churn', df_load['Churn'].isnull().sum())
+df_load.dropna(subset=['Churn'],inplace=True)
+print('total kolom data yang di hapus missing value', df_load.shape)
+
+#pengisian nilai missing value pada nilai tertentu
+print('Status Missing Values :', df_load.isnull().values.any())
+print('\n Jumlah Missing Values masing-masing kolom, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+
+# handling missing values Tenure fill with 11
+df_load['tenure'].fillna(11, inplace=True)
+
+# Handling missing values num vars (except Tenure)
+for col_name in list(['MonthlyCharges', 'TotalCharges']):
+    median = df_load[col_name].median()
+    df_load[col_name].fillna(median, inplace=True)
+
+print('\nJumlah Missing Values setelah di imputer datanya, adalah:')
+print(df_load.isnull().sum().sort_values(ascending=False))
+#mendeteksi adanya boxplot
+print('\nPersebaran data sebelum ditangani Outlier: ')
+print(df_load[['tenure','MonthlyCharges','TotalCharges']].describe())
+plt.figure()
+sns.boxenplot(x=df_load['tenure'])
+plt.show()
+
+plt.figure()
+sns.boxenplot(x=['MonthlyCharges'])
+plt.show()
+
+plt.figure()
+sns.boxenplot(x=['TotalCharges'])
+plt.show()
+
+# mengatasi outlier with IQR
+Q1 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.25)
+Q3 = (df_load[['tenure','MonthlyCharges','TotalCharges']]).quantile(0.75)
+
+IQR = Q3 - Q1
+maximum = Q3 + (1.5*IQR)
+print('Nilai Maximum dari masing-masing Variable adalah: ')
+print(maximum)
+minimum = Q1 - (1.5*IQR)
+print('\nNilai Minimum dari masing-masing Variable adalah: ')
+print(minimum)
+
+more_than = (df_load > maximum)
+lower_than = (df_load < minimum)
+df_load = df_load.mask(more_than, maximum, axis=1)
+df_load = df_load.mask(lower_than, minimum, axis=1)
+
+print('\nPersebaran data setelah ditangani Outlier: ')
+print(df_load[['tenure','MonthlyCharges','TotalCharges']].describe())
+
+# Masukkan variable
+for col_name in list(['gender','SeniorCitizen','Partner','Dependents','PhoneService','MultipleLines','InternetService','OnlineSecurity','OnlineBackup','DeviceProtection','TechSupport','StreamingTV','StreamingMovies','Contract','PaperlessBilling','PaymentMethod','Churn']):
+    print('\nUnique Values Count \033[1m' + 'Before Standardized \033[0m Variable',col_name)
+    print(df_load[col_name].value_counts())
+
+df_load = df_load.replace(['Wanita','Laki-Laki','Churn','Iya'],['Female','Male','Yes','Yes'])
+for col_name in list(['gender','Dependents','Churn']):
+    print('\nUnique Values Count \033[1m' + 'After Standardized \033[0mVariable', col_name)
+    print(df_load[col_name].value_counts())
